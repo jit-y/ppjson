37fc82dbff732dacbb728d23ecc6cb7acf845099
YUJI takahashi <jit.y.31@gmail.com>
2019-01-13 18:08:08 +0900

    Imprement print slice

--- ppjson.go
+++ ppjson.go
@@ -3,6 +3,7 @@ package ppjson
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -12,9 +13,10 @@ import (
 )
 
 type printer struct {
+	decoder *json.Decoder
 	reader  io.Reader
 	writer  io.Writer
-	value   interface{}
+	depth   int
 	indent  int
 	newLine string
 }
@@ -32,10 +34,14 @@ func Format(data []byte) ([]byte, error) {
 }
 
 func NewPrinter(reader io.Reader, writer io.Writer) *printer {
+	dec := json.NewDecoder(reader)
+
 	return &printer{
+		decoder: dec,
 		reader:  reader,
 		writer:  writer,
 		indent:  2,
+		depth:   0,
 		newLine: "\n",
 	}
 }
@@ -51,8 +57,7 @@ func (p *printer) Write(b []byte) (int, error) {
 }
 
 func (p *printer) Pretty() (string, error) {
-	dec := json.NewDecoder(p.reader)
-	t, err := dec.Token()
+	t, err := p.decoder.Token()
 	if err != nil {
 		return "", err
 	}
@@ -73,6 +78,8 @@ func (p *printer) format(v interface{}) (string, error) {
 		return "null", nil
 	case float64:
 		return strconv.FormatFloat(val, 'f', -1, 64), nil
+	case json.Delim:
+		return p.formatSlice(val)
 	default:
 		k := reflect.ValueOf(val).Kind()
 		return "", fmt.Errorf("parse failed: type %v is not supported", k)
@@ -89,4 +96,66 @@ func (p *printer) formatString(val string) (string, error) {
 	}
 
 	return strings.TrimRight(writer.String(), "\n"), nil
+}
+
+func (p *printer) formatSlice(d json.Delim) (string, error) {
+	var b strings.Builder
+	b.WriteString(p.stringWithIndent(d.String() + p.newLine))
+
+	err := p.withIndent(func() error {
+		for i := 0; p.decoder.More(); i++ {
+			var v interface{}
+
+			err := p.decoder.Decode(&v)
+			if err != nil {
+				return err
+			}
+
+			val, err := p.format(v)
+			if err != nil {
+				return err
+			}
+
+			if i > 0 {
+				b.WriteString("," + p.newLine)
+			}
+
+			b.WriteString(p.stringWithIndent(val))
+
+		}
+
+		return nil
+	})
+
+	token, err := p.decoder.Token()
+	if err != nil {
+		return "", err
+	}
+
+	delim, ok := token.(json.Delim)
+	if !ok {
+		return "", errors.New("invalid format")
+	}
+
+	b.WriteString(p.stringWithIndent(p.newLine + delim.String()))
+
+	return b.String(), nil
+}
+
+func (p *printer) currentIndent() string {
+	return strings.Repeat(" ", p.indent*p.depth)
+}
+
+func (p *printer) stringWithIndent(val string) string {
+	return p.currentIndent() + val
+}
+
+func (p *printer) withIndent(fn func() error) error {
+	p.depth++
+
+	err := fn()
+
+	p.depth--
+
+	return err
 }
--- ppjson_test.go
+++ ppjson_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 type testData struct {
+	desc     string
 	data     []byte
 	expected string
 }
@@ -17,16 +18,16 @@ type testData struct {
 func TestString(t *testing.T) {
 	tests := buildTestData()
 
-	for i, test := range tests {
+	for _, test := range tests {
 		buf := bytes.NewBuffer(test.data)
 		p := ppjson.NewPrinter(buf, os.Stdout)
 		actual, err := p.Pretty()
 		if err != nil {
-			t.Errorf("tests[%d] parse error. %v", i, err)
+			t.Errorf("test %s: %v", test.desc, err)
 		}
 
 		if test.expected != actual {
-			t.Errorf("tests[%d] wrong. expected=%s, got=%s", i, test.expected, actual)
+			t.Errorf("test %s: wrong. expected=%s, got=%s", test.desc, test.expected, actual)
 		}
 	}
 }
@@ -34,16 +35,28 @@ func TestString(t *testing.T) {
 func buildTestData() []testData {
 	return []testData{
 		{
+			desc:     "string",
 			data:     toJson("test"),
 			expected: "\"test\"",
 		},
 		{
+			desc:     "nil",
 			data:     toJson(nil),
 			expected: "null",
 		},
 		{
+			desc:     "int",
 			data:     toJson(1234567890),
 			expected: "1234567890",
+		},
+		{
+			desc: "array",
+			data: toJson([]interface{}{1, "111", nil}),
+			expected: `[
+  1,
+  "111",
+  null
+]`,
 		},
 	}
 }
